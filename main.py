6df38035ba20df20b84b5c1ef02e9c4408fcb492
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-06-26 12:37:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -119,10 +119,12 @@ def match_progression(x_df,match_id,pipe):
     print("Target-",target)
     temp_df = temp_df[['end_of_over','runs_after_over','wickets_in_over','lose','win']]
     return temp_df,target
+st.title('Analysis of Previous Matches')
 g=delivery_df['match_id'].unique()
 b=st.selectbox('Match_id',g)
 temp_df,target = match_progression(delivery_df,b,pipe)
 st.subheader(delivery_df[delivery_df['match_id']==b]['batting_team'].unique()+' v/s '+delivery_df[delivery_df['match_id']==b]['bowling_team'].unique())
+st.write(delivery_df[delivery_df['match_id']==b]['city'].unique())
 import plotly.graph_objects as go
 if a==0:
     fig = go.Figure()
