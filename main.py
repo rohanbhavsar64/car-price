38ee85ebcf97f13a874233906f1fa233c57402b7
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-06-26 12:20:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def match_progression(x_df,match_id,pipe):
     print("Target-",target)
     temp_df = temp_df[['end_of_over','runs_after_over','wickets_in_over','lose','win']]
     return temp_df,target
-b=st.number_input('Match_id')
+b=st.select_box('Match_id',delivery_df['match_id'].unique())
 temp_df,target = match_progression(delivery_df,b,pipe)
 temp_df
 import plotly.graph_objects as go
