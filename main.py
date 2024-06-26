e13528c25b28f18a0c817be4920786c14496fe10
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-06-26 12:02:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,5 +123,5 @@ batting_team=fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win']
 bowling_team=fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', line=dict(color='red', width=4)))
 runs=fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over']))
 fig.update_layout(title='Target-' + str(target))
-fig.show()
+st.write(fig.show())
 
