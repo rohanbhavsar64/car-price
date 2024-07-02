401e9a1c659e53dba528a3fefbb9c3622538fd0f
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-02 19:55:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ if a!= b:
     fig = go.Figure()
 
     fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['wickets_in_over'], mode='markers', name='Wickets in Over', marker=dict(color='yellow')))
-    fig.add_trace(go.bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'],name='Runs per over', marker=dict(color='purple')))
+    fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'],marker=dict(color='purple')))
     fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting Team Probability', line=dict(color='#00a65a', width=4)))
     fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name='Bowling Team Probability', line=dict(color='red', width=4)))
 
