074193a3191e43bbb1e6f3b059f7247b9c32dcf3
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-02 20:04:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -132,11 +132,18 @@ import plotly.graph_objects as go
 if a!= b:
     fig = go.Figure()
 
+    fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over', marker=dict(color='purple')))
     fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['wickets_in_over'], mode='markers', name='Wickets in Over', marker=dict(color='yellow')))
-    fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'],name='runs in Over',marker=dict(color='purple')))
-    fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting Team Probability', line=dict(color='#00a65a', width=4)))
+    fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['win'], mode='lines', name='Batting Team Probability', line=dict(color='#00a65a', width=3)))
     fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=temp_df['lose'], mode='lines', name='Bowling Team Probability', line=dict(color='red', width=4)))
 
-    fig.update_layout(title='Target-' + str(target), legend_title='Legend')
+    fig.update_layout(title='Target-' + str(target),
+                      xaxis_title='End of Over',
+                      yaxis_title='Runs in Over',
+                      yaxis2_title='Wickets in Over',
+                      yaxis2_overlaying='y',
+                      yaxis2_side='right')
 
-    st.plotly_chart(fig)
+    fig.update_yaxes(range=[0, max(temp_df['wickets_in_over']) + 1], secondary_y=True)
+
+    st.write(fig)
