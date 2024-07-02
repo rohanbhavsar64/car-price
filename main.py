2f14fdd5a3fb7b1d61df34ee6e41031282d94a82
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-02 20:41:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ if part == "Prediction":
     batting=final_df['batting_team'].unique()
     shar=final_df['city'].unique()
     wic2=[1,2,3,4,5,6,7,8,9,10]
+    wic1=[0,1,2,3,4,5,6,7,8,9,10]
     col1,col2,col3=st.columns(3)
     with col1:
         a = st.selectbox('batting_team',sorted(batting))
