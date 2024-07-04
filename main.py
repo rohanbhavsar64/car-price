2e466a3c10468d45ccbe480a6c1122336eb1d35b
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-04 13:07:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -141,5 +141,8 @@ elif part == "Analysis":
             r3 = match[match['id'] == l]['venue'].unique()
             r4 = match[match['id'] == l]['city'].unique()
             r5 = match[match['id'] == l]['toss_winner'].unique()
-            r6 = match[
+            r6 = match[match['id'] == l]['dl_applied'].unique() 
+            data = {'Field': ['Vanue', 'City', 'Toss Winner', 'DLS Methode', 'POM', 'Winner'], 'Name': [r3[0], r4[0], r5[0], r6[0], r1[0], r2[0]]} 
+            fg = pd.DataFrame(data) 
+            st.table(fg)
         
