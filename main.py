d016ca4a29eb3386f236a24da3ad8f5a4179a652
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2024-07-05 10:08:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -128,14 +128,14 @@ elif part == "Analysis":
             st.write("Error: Match is not Existed")
         else:
             fig = go.Figure()
-            runs = fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over'))
+            runs = fig.add_trace(go.Bar(x=temp_df['end_of_over'], y=temp_df['runs_after_over'], name='Runs in Over',marker=dict(color='purple')))
             wicket_text = temp_df['wickets_in_over'].astype(str)
             wicket_y = temp_df['runs_after_over'] + temp_df['wickets_in_over'] * 1  # adjust y-position based on wickets
             wicket_y[wicket_y == temp_df['runs_after_over']] = None  # hide scatter points for 0 wickets
 
             wicket = fig.add_trace(go.Scatter(x=temp_df['end_of_over'], y=wicket_y,  # use adjusted y-position
                                                mode='markers', name='Wickets in Over',
-                                               marker=dict(color='yellow', size=10),
+                                               marker=dict(color='orange', size=10),
                                                text=wicket_text, textposition='top center'))
 
 # Line plots for batting and bowling teams
